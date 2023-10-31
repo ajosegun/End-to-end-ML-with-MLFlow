1bfd791be295cdbcc556c60ec4546d0d3709e756
Olusegun Ajose <ajoseolusegun@gmail.com>
2023-10-31 07:00:25 +0100

    Adding exception and logger

--- template.py
+++ template.py
@@ -11,7 +11,7 @@ list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/utils/common.py",  # for frequently used functions
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
@@ -27,6 +27,7 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "notebook/trials.py",
+    "notebook/trials.ipynb",
     "templates/index.html",
     # "test.py"
 ]
